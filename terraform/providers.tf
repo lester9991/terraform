e480ffcd04b3b9ec3403eb2b3c891871190943f8
lester9991 <lesterdsouza820@gmail.com>
2026-06-13 15:49:18 +0530

    update

--- terraform/providers.tf
+++
@@ -1,4 +0,0 @@
-provider "google" {
-  project     = "spartan-shadow-438812-q6"
-  region      = "asia-south1"
-}
--- terraform/users.tf
+++
@@ -1,5 +0,0 @@
-resource "google_project_iam_member" "project_viewer_user" {
- project = "spartan-shadow-438812-q6"
- role    = "roles/viewer"
- member  = "user:saple998@gmail.com"
-}
